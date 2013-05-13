960348aaffda495dd4680008a0909fe29bb2ef48
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-05-13 15:18:25 +0200

    Update Base controller to not depend on Play Scala 1.x

--- modules/swagger-play/app/controllers/ApiHelpController.java
+++ modules/swagger-play/app/controllers/ApiHelpController.java
@@ -14,23 +14,24 @@
  *  limitations under the License.
  */
 
-package controllers
+package controllers;
 
-import play.mvc.Controller
-import play.modules.swagger.ApiHelpInventory
-import play.Logger
+import play.mvc.Controller;
+
+import play.modules.swagger.ApiHelpInventory;
+import play.Logger;
 
 /**
   * This controller exposes swagger compatiable help apis.<br/>
-  * The routing for the two apis supported by this controller is automatically injected by SwaggerPlugin
+  * The routing for the two apis supported by this controller is automatically injected by swagger plugin
   *
   * @author ayush
   * @since 10/9/11 4:37 PM
   *
   */
-object ApiHelpController extends Controller {
-  def catchAll() = {
-    Logger.info("ApiHelpController.catchAll got called. This should not happen; SwaggerPlugin.rawInvocation should be intercepting and processing this call")
+public class ApiHelpController extends Controller {
+  public static void catchAll() {
+    Logger.info("ApiHelpController.catchAll got called. This should not happen; SwaggerPlugin.rawInvocation should be intercepting and processing this call");
 
   }
 }
