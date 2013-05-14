69807c1ae6d931fee64ad4d98350a569fdb39d27
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-05-14 12:09:33 +0200

    Add ways to customize paths

--- modules/swagger-play/src/play/modules/swagger/SwaggerPlugin.java
+++ modules/swagger-play/src/play/modules/swagger/SwaggerPlugin.java
@@ -38,8 +38,9 @@ public class SwaggerPlugin extends PlayPlugin {
     private static final String APPLICATION_XML = "application/xml";
     private static final String UTF_8 = "UTF-8";
     private static final String XML = ".xml";
-    private static final String RESOURCES_XML = "/resources.xml";
-    private static final String RESOURCES_JSON = "/resources.json";
+    private static final String RESOURCES_XML = play.Play.configuration.getProperty("swagger.api.resPath", "/resources") + XML;
+    private static final String RESOURCES_JSON = play.Play.configuration.getProperty("swagger.api.resPath", "/resources") + JSON;
+    private static final String PREFIX = play.Play.configuration.getProperty("swagger.api.prefix", "");
 
     @Override
     public boolean rawInvocation(Request request, Response response) throws Exception {
@@ -48,22 +49,22 @@ public class SwaggerPlugin extends PlayPlugin {
         try {
             final List<String> resourceNames = ApiHelpInventory.getResourceNames();
             for (String resourceName : resourceNames) {
-                if (request.path.equals(resourceName + JSON)) {
+                if (request.path.equals(PREFIX + resourceName + JSON)) {
                     response.contentType = APPLICATION_JSON;
                     String apiHelp = ApiHelpInventory.getPathHelpJson(resourceName);
                     response.out.write(apiHelp.getBytes(UTF_8));
                     return true;
-                } else if (request.path.equals(resourceName + XML)) {
+                } else if (request.path.equals(PREFIX + resourceName + XML)) {
                     response.contentType = APPLICATION_XML;
                     String apiHelp = ApiHelpInventory.getPathHelpXml(resourceName);
                     response.out.write(apiHelp.getBytes(UTF_8));
                     return true;
-                } else if (request.path.equals(RESOURCES_JSON)) {
+                } else if (request.path.equals(PREFIX + RESOURCES_JSON)) {
                     response.contentType = APPLICATION_JSON;
                     String apiHelp = ApiHelpInventory.getRootHelpJson(resourceName);
                     response.out.write(apiHelp.getBytes(UTF_8));
                     return true;
-                } else if (request.path.equals(RESOURCES_XML)) {
+                } else if (request.path.equals(PREFIX + RESOURCES_XML)) {
                     response.contentType = APPLICATION_XML;
                     String apiHelp = ApiHelpInventory.getRootHelpXml(resourceName);
                     response.out.write(apiHelp.getBytes(UTF_8));
@@ -83,16 +84,16 @@ public class SwaggerPlugin extends PlayPlugin {
         final List<String> resourceNames = ApiHelpInventory.getResourceNames();
 
         if (resourceNames.size() > 0) {
-            Router.prependRoute("GET", RESOURCES_XML, "ApiHelpController.catchAll");
-            Router.prependRoute("GET", RESOURCES_JSON, "ApiHelpController.catchAll");
-            Logger.info("Swagger: Added ROOT help api @ " + RESOURCES_JSON);
-            Logger.info("Swagger: Added ROOT help api @ " + RESOURCES_XML);
+            Router.prependRoute("GET", PREFIX + RESOURCES_XML, "ApiHelpController.catchAll");
+            Router.prependRoute("GET", PREFIX + RESOURCES_JSON, "ApiHelpController.catchAll");
+            Logger.info("Swagger: Added ROOT help api @ " + PREFIX + RESOURCES_JSON);
+            Logger.info("Swagger: Added ROOT help api @ " + PREFIX + RESOURCES_XML);
             for (String resourceName : resourceNames) {
                 Router.prependRoute("GET", resourceName + ".json", "ApiHelpController.catchAll");
                 Router.prependRoute("GET", resourceName + ".xml", "ApiHelpController.catchAll");
 
-                Logger.info("Swagger: Added help api @ " + resourceName + ".json");
-                Logger.info("Swagger: Added help api @ " + resourceName + ".xml");
+                Logger.info("Swagger: Added help api @ " + PREFIX + resourceName + ".json");
+                Logger.info("Swagger: Added help api @ " + PREFIX + resourceName + ".xml");
             }
         }
     }
